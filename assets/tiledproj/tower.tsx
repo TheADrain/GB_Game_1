da02c105616412d185fe3c652afe867c69700165
TheADrain <theadrain@hotmail.co.uk>
2021-02-03 19:08:26 +0000

    - fixed tower tile exports

--- assets/tiledproj/tower.tsx
+++ assets/tiledproj/tower.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="tower" tilewidth="8" tileheight="8" tilecount="64" columns="16">
- <image source="tower_tiledata.bmp" width="128" height="32"/>
+<tileset version="1.4" tiledversion="1.4.3" name="tower" tilewidth="8" tileheight="8" tilecount="128" columns="8">
+ <image source="../art/tsetTower.png" width="64" height="128"/>
 </tileset>
--- src/tiles/tower_tiledata.h
+++ src/tiles/tower_tiledata.h
@@ -6,7 +6,7 @@
     Format Gameboy 2bpp interleaved
     Compression None
     Tile Size 8x8
-    Number of Tiles 0 to 59
+    Number of Tiles 0 to 86
 
     Palette: None
     SGB Palette: None
@@ -16,6 +16,6 @@
 
 
 // Tilemap Data Length in tiles (16 bytes each)
-#define tower_tiledataLength 60
+#define tower_tiledataLength 87
 // Tilemap Data (one 16-byte tile per line)
-extern const unsigned char tower_tiledata[960];
+extern const unsigned char tower_tiledata[1392];
