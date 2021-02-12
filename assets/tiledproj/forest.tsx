07858dee943a227e757165c285f512036a7430be
TheADrain <theadrain@hotmail.co.uk>
2021-02-12 14:37:24 +0000

    - cleaned up the tiled proj and sprites

--- assets/tiledproj/forest.tsx
+++ assets/tiledproj/forest.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="forest" tilewidth="8" tileheight="8" tilecount="368" columns="16">
- <image source="../art/DeepForestTileset.png" width="128" height="184"/>
+<tileset version="1.4" tiledversion="1.4.3" name="forest" tilewidth="8" tileheight="8" tilecount="274" columns="16">
+ <image source="../art/tset_forest.png" width="128" height="136"/>
  <tile id="320">
   <properties>
    <property name="type" value="kill"/>
--- assets/tiledproj/tower.tsx
+++ assets/tiledproj/tower.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="tower" tilewidth="8" tileheight="8" tilecount="128" columns="8">
- <image source="../art/tsetTower.png" width="64" height="128"/>
+ <image source="../art/tset_tower.png" width="64" height="128"/>
 </tileset>
